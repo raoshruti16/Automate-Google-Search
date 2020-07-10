462570eab9a31cba5ad5d39dd8efba066b9b16f8
Shruti Rao <48805685+raoshruti16@users.noreply.github.com>
2020-07-10 20:13:45 +0530

    Update automate.py

--- automate.py
+++ automate.py
@@ -6,9 +6,13 @@ This is a temporary script file.
 """
 
 from selenium import webdriver
+import time
 browser = webdriver.Chrome('C:\chromedriver_win32\chromedriver')
 browser.get('http://www.google.com') 
 elem = browser.find_element_by_name('q')
-elem.send_keys('Akshay Kumar')
+elem.send_keys('Continuum')
 from selenium.webdriver.common.keys import Keys
 elem.send_keys(Keys.ENTER)
+time.sleep(3)
+link = browser.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div[1]/a/h3')
+link.click()
